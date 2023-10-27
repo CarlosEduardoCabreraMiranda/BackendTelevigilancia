2002e45d0ddc91e6120287a04b9a70c94a67851d
CarlosEduardoCabreraMiranda <cecabrera-2022a@corhuila.edu.co>
2023-10-26 21:28:42 -0500

    backend, para solucion de errores 1.0

---
+++ src/main/java/com/example/TvBackend/New/pojo/Producto.java
@@ -0,0 +1,48 @@
+package com.example.TvBackend.New.pojo;
+
+import lombok.Data;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serializable;
+
+
+@NamedQuery(name="Producto.findByCodigo_producto", query = "select p from Producto p where p.codigo=:codigo")
+
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Data
+@Table (name = "producto")
+public class Producto implements Serializable {
+    private static final long serialversionUID =1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+
+    @Column(name = "codigo")
+    private String codigo;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "costo")
+    private float costo;
+
+    @Column(name = "tipo")
+    private String tipo;
+
+    public Producto(){}
+
+    public Producto(Integer id, String codigo, String nombre, float costo, String tipo) {
+        this.id = id;
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.costo = costo;
+        this.tipo = tipo;
+    }
+}
