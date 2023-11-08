2f2d2ca8205a48866f38901f21d00d0fd63679c7
CarlosEduardoCabreraMiranda <cecabrera-2022a@corhuila.edu.co>
2023-11-08 06:55:03 -0500

    Borrando contenido para  actualizar repositorio a nueva arquitectura

--- src/main/java/com/example/TvBackend/Repository/IProducto.java
+++
@@ -1,8 +0,0 @@
-package com.example.TvBackend.Repository;
-
-import com.example.TvBackend.Model.Producto;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IProducto  extends CrudRepository<Producto,Integer>{
-
-}
